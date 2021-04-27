e578cc3756d9b7bed6f0cfae91ad9d3da48d069e
Martien Dermawan Tanama <martiendt@gmail.com>
2021-04-27 09:45:47 +0700

    add gtag

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./index.css";
+import VueGtag from "vue-gtag-next";
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(VueGtag, {
+    property: {
+      id: process.env.VUE_APP_GTAG_ID
+    }
+  })
   .mount("#app");
